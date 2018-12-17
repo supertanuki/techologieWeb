6e4fad80cca86a9edd7393dd1534005bae258436
Richard Hanna <supertanuki@gmail.com>
2018-12-17 23:50:29 +0100

    Add dark mode

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+function toggleMode() {
+    var body = document.querySelector('body');
+    var mode = body.className === 'dark' ? '' : 'dark';
+    body.className = mode;
+    setCookie('mode', mode, 100);
+}
+
+function setCookie(name, value, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = name + "=" + value + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+if ('dark' !== getCookie('mode')) {
+    toggleMode();
+}
