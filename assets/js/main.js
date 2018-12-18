1ad888f40c048d4a9ec800c44e8246cace1f7ced
Richard Hanna <supertanuki@gmail.com>
2018-12-18 20:52:20 +0100

    Use localstorage to store mode

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,10 @@
 function toggleMode() {
     var body = document.querySelector('body');
-    var mode = body.className === 'dark' ? '' : 'dark';
+    var mode = body.className === 'dark' ? 'light' : 'dark';
     body.className = mode;
-    setCookie('mode', mode, 100);
+    localStorage.setItem('mode', mode);
 }
 
-function setCookie(name, value, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-    var expires = "expires=" + d.toUTCString();
-    document.cookie = name + "=" + value + ";" + expires + ";path=/";
-}
-
-function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
-
-if ('dark' !== getCookie('mode')) {
+if ('light' === sessionStorage.getItem('mode')) {
     toggleMode();
 }
