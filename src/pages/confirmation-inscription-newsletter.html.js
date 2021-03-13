bf80f86a5fa0d242f15246fb8ff7d5fb6d448a51
Richard Hanna <supertanuki@users.noreply.github.com>
2021-03-13 22:53:52 +0100

    Fix sentences

--- src/pages/confirmation-inscription-newsletter.html.js
+++ src/pages/confirmation-inscription-newsletter.html.js
@@ -9,13 +9,13 @@ export default () => (
     <h1>C'est tout bon, vous êtes inscrit·e !</h1>
 
     <p>
-      Vous allez avoir le plaisir, nous espérons, de recevoir toutes les 2 semaines un contenu original, une veille collaborative sur le thème technologies et écologie, des alertes greenwashing et le tout sur un ton léger car oui, même si le sujet est grave, on essaye de ne pas se prendre au sérieux.
+      Vous allez avoir le plaisir, nous espérons, de recevoir toutes les 2 semaines un contenu original, une veille collaborative sur le thème technologies et écologie mais aussi des alertes au greenwashing !
     </p>
   
     <p>Vous pourrez vous désinscrire à tout moment.</p>
   
     <p>
-      En attendant, et si ce n'est pas encore fait, rejoignez-nous sur notre <a href="https://join.slack.com/t/techologie/shared_invite/enQtNTMwODc1NTYxNDkxLTMzZWI3NTIwNWUyMzIwMGIyMzY2YmYyNDFiZWMyOWJiMTJjOWRkODg2MWM4Y2M1OWE4Y2Y2MmJiNjQ5NTcwZmM">communauté slack</a>.
+      En attendant, et si ce n'est pas encore fait, rejoignez-nous sur notre <a href="https://join.slack.com/t/techologie/shared_invite/enQtNTMwODc1NTYxNDkxLTMzZWI3NTIwNWUyMzIwMGIyMzY2YmYyNDFiZWMyOWJiMTJjOWRkODg2MWM4Y2M1OWE4Y2Y2MmJiNjQ5NTcwZmM">communauté Techologie sur slack</a>.
     </p>
   </Layout>
 )
